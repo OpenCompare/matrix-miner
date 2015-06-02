7f79ac19bf4947f3867144fc202e7a50a187f718
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-06-02 11:46:21 +0200

    stupid angular example

--- public/javascripts/overview-controller.js
+++ public/javascripts/overview-controller.js
@@ -2,7 +2,13 @@
  * Created by gbecan on 6/2/15.
  */
 
-matrixMinerApp.controller("OverviewController", function($rootScope, $scope, $http, uiGridConstants) {
+matrixMinerApp.controller("OverviewController", function($rootScope, $scope, $http) {
 
     $scope.message = "toto";
+
+    $scope.overviews = ["toto", "tata"];
+
+    $scope.search = function(cell, feature) {
+
+    }
 });
