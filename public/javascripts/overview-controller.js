e5087855f9c7ca4542f74ef461baf207a82dc024
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-06-02 11:42:24 +0200

    controller for overview

---
+++ public/javascripts/overview-controller.js
@@ -0,0 +1,8 @@
+/**
+ * Created by gbecan on 6/2/15.
+ */
+
+matrixMinerApp.controller("OverviewController", function($rootScope, $scope, $http, uiGridConstants) {
+
+    $scope.message = "toto";
+});
