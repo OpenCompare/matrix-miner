5ea9b2b376fe589e983e5904337c476bf34e9afb
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-07-06 17:52:34 +0200

    load PCM and create form for all cells

--- public/javascripts/EvalCtrl.js
+++ public/javascripts/EvalCtrl.js
@@ -2,42 +2,53 @@
  * Created by gbecan on 7/6/15.
  */
 
-matrixMinerApp.controller("EvalCtrl", function($rootScope, $scope, $http, $window, embedService) {
+matrixMinerApp.controller("EvalCtrl", function($rootScope, $scope, $http, $window, embedService, base64, pcmApi) {
 
     // Configure OpenCompare editor
     embedService.enableEdit(false).set();
     embedService.enableShare(false).set();
     embedService.enableExport(false).set();
 
+
+    // Init
+    var pcmMM = Kotlin.modules['pcm'].pcm;
+    var factory = new pcmMM.factory.DefaultPcmFactory();
+    var loader = factory.createJSONLoader();
+
+    $scope.feature = {};
+    $scope.cells = [];
+
     // Load PCM
     $http.get("/eval/load/" + dirPath).success(function (data) {
-        embedService.initialize(data);
+        // Initialize other controllers
+        embedService.initialize(data); // TODO : display only the necessary feature
         $rootScope.$broadcast("overviews", data.overviews);
         $rootScope.$broadcast("specifications", data.specifications);
+
+        // Prepare evaluation
+        var pcm = loader.loadModelFromString(JSON.stringify(data.pcm)).array[0];
+        pcmApi.decodePCM(pcm);
+
+
+        pcm.features.array.forEach(function (feature) {
+            // TODO : display only the necessary feature
+            //if (feature.name === evaluatedFeature) {
+                $scope.feature.name = feature.name;
+            //}
+        });
+
+        pcm.products.array.forEach(function (product) {
+            $scope.cells.push({
+                name: "tata",
+                product : product.name
+            });
+        });
     });
-
-    $scope.completed = false;
-
-    $scope.feature = {
-        name: "feature"
-    };
-
-    $scope.cells = [
-        {
-            name: "cell1"
-        },
-        {
-            name: "cell2"
-        },
-        {
-            name: "cell3"
-        }
-    ];
 
     $scope.send = function() {
 
         var evalResults = {
-            pcm : "id",
+            pcm : dirPath,
             feature : $scope.feature,
             cells : $scope.cells
         };
